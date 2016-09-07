c1c2c11a95b6919a44b6e3e5c52bf4656580820d
pratik <pratiksonii@yahoo.in>
2016-09-07 09:42:37 +0530

    added @RestController support

--- SpringBootAspect/src/main/java/com/pratik/SpringBootAspectApplication.java
+++ SpringBootAspect/src/main/java/com/pratik/SpringBootAspectApplication.java
@@ -2,8 +2,10 @@ package com.pratik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+//@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class SpringBootAspectApplication {
 
 	public static void main(String[] args) {
--- SpringBootAspect/src/main/java/com/pratik/aspect/LoggingAspect.java
+++ SpringBootAspect/src/main/java/com/pratik/aspect/LoggingAspect.java
@@ -4,18 +4,25 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.aop.ThrowsAdvice;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class LoggingAspect implements ThrowsAdvice{
+public class LoggingAspect{
 
 	@Pointcut("execution(* com..*service.*(..))")
 	private void selectAll(){}
+//	@Pointcut("execution(* com..*controller.*(..))")
+	@Pointcut("within(@org.springframework.web.bind.annotation.RestController *)")
+	private void selectController(){}
 
 	@Before("selectAll()")
 	public void beforeAdvice(JoinPoint j){
+		System.out.println("--- Start method: "+ j.getSignature().getName() );
+	}
+
+	@Before("selectController()")
+	public void beforeControllerAdvice(JoinPoint j){
 		System.out.println("--- Start method: "+ j.getSignature().getName() );
 	}
 }
